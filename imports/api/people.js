8c1aeef2165767fe47db4a9efd5d46f2810f18a2
Valentin Durvenqkov <Valka7a@Valentins-MacBook-Pro.local>
2016-12-13 18:10:07 +0200

    Fix inserting database.
    Signed-off-by: Valentin Durvenqkov <Valka7a@Valentins-MacBook-Pro.local>

--- imports/api/people.js
+++ imports/api/people.js
@@ -17,32 +17,32 @@ People.schema = new SimpleSchema({
 	}
 });
 
-Meteor.startup(() => {
-    if (People.find().count() == 0) {
-        People.insert({ firstName: "Sang" , lastName: "Cardone", age: 33, createdAt: new Date() });
-		People.insert({ firstName: "Ruthann" , lastName: "Jablonski", age: 32, createdAt: new Date() });
-		People.insert({ firstName: "Garland" , lastName: "Oatman", age: 31, createdAd: new Date() });
-		People.insert({ firstName: "Abdul" , lastName: "Paull", age: 31, createdAd: new Date() });
-		People.insert({ firstName: "Stasia" , lastName: "Olivarez", age: 30, createdAd: new Date() });
-		People.insert({ firstName: "Tamie" , lastName: "Archer", age: 30, createdAd: new Date() }); 
-		People.insert({ firstName: "Racquel" , lastName: "Navarrete", age: 29, createdAd: new Date() });
-		People.insert({ firstName: "Tandra" , lastName: "Higuchi", age: 29, createdAd: new Date() });
-		People.insert({ firstName: "Scot" , lastName: "Poulton", age: 28, createdAd: new Date() });
-		People.insert({ firstName: "Kathryn" , lastName: "Precourt", age: 28, createdAd: new Date() });
-		People.insert({ firstName: "Stacy" , lastName: "Zeck", age: 27, createdAd: new Date() });
-		People.insert({ firstName: "Trista" , lastName: "Joyce", age: 27, createdAd: new Date() });
-		People.insert({ firstName: "Eleonora" , lastName: "Thetford", age: 26, createdAd: new Date() });
-		People.insert({ firstName: "Gwen" , lastName: "Herrada", age: 26, createdAd: new Date() });
-		People.insert({ firstName: "Sarita" , lastName: "Kung", age: 25, createdAd: new Date() });
-		People.insert({ firstName: "Paul" , lastName: "Tanouye", age: 25, createdAd: new Date() });
-		People.insert({ firstName: "Emmanuel" , lastName: "Devinney", age: 24, createdAd: new Date() });
-		People.insert({ firstName: "Karima" , lastName: "Schuster", age: 24, createdAd: new Date() });
-		People.insert({ firstName: "Salena" , lastName: "Clewis", age: 23, createdAd: new Date() });
-		People.insert({ firstName: "Carleen" , lastName: "Corrie", age: 23, createdAd: new Date() });
-    }
-});
-
 if (Meteor.isServer) {
+	Meteor.startup(() => {
+		if (People.find().count() == 0) {
+		    People.insert({ firstName: "Sang" , lastName: "Cardone", age: 33, createdAt: new Date() });
+			People.insert({ firstName: "Ruthann" , lastName: "Jablonski", age: 32, createdAt: new Date() });
+			People.insert({ firstName: "Garland" , lastName: "Oatman", age: 31, createdAd: new Date() });
+			People.insert({ firstName: "Abdul" , lastName: "Paull", age: 31, createdAd: new Date() });
+			People.insert({ firstName: "Stasia" , lastName: "Olivarez", age: 30, createdAd: new Date() });
+			People.insert({ firstName: "Tamie" , lastName: "Archer", age: 30, createdAd: new Date() }); 
+			People.insert({ firstName: "Racquel" , lastName: "Navarrete", age: 29, createdAd: new Date() });
+			People.insert({ firstName: "Tandra" , lastName: "Higuchi", age: 29, createdAd: new Date() });
+			People.insert({ firstName: "Scot" , lastName: "Poulton", age: 28, createdAd: new Date() });
+			People.insert({ firstName: "Kathryn" , lastName: "Precourt", age: 28, createdAd: new Date() });
+			People.insert({ firstName: "Stacy" , lastName: "Zeck", age: 27, createdAd: new Date() });
+			People.insert({ firstName: "Trista" , lastName: "Joyce", age: 27, createdAd: new Date() });
+			People.insert({ firstName: "Eleonora" , lastName: "Thetford", age: 26, createdAd: new Date() });
+			People.insert({ firstName: "Gwen" , lastName: "Herrada", age: 26, createdAd: new Date() });
+			People.insert({ firstName: "Sarita" , lastName: "Kung", age: 25, createdAd: new Date() });
+			People.insert({ firstName: "Paul" , lastName: "Tanouye", age: 25, createdAd: new Date() });
+			People.insert({ firstName: "Emmanuel" , lastName: "Devinney", age: 24, createdAd: new Date() });
+			People.insert({ firstName: "Karima" , lastName: "Schuster", age: 24, createdAd: new Date() });
+			People.insert({ firstName: "Salena" , lastName: "Clewis", age: 23, createdAd: new Date() });
+			People.insert({ firstName: "Carleen" , lastName: "Corrie", age: 23, createdAd: new Date() });		
+		}
+	});
+
 	People._ensureIndex({
 		firstName: 1,
 		lastName: 1,
